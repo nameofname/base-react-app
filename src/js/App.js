f06c5807132fdbf8419ea7e9e2e4a6cc5d80b19b
nameofname <nameofemail@gmail.com>
2018-01-13 16:47:26 -0500

    lightning and winky face flash now

--- src/js/App.js
+++ src/js/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
-import lightning from '../img/lightning.svg';
-import wink from '../img/wink.svg';
 import '../css/App.css';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={lightning} className="App-logo lightning" />
-          <img src={wink} className="App-logo wink" />
-          <h1 className="App-title">Welcome to you, looking at this page.</h1>
-        </header>
+        <Header />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
