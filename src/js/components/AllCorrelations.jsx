db7f1904a1e8f93ad922fd1d6c5615a22540b260
nameofname <nameofemail@gmail.com>
2018-01-14 18:12:01 -0500

    properly formatted table

--- src/js/components/AllCorrelations.jsx
+++ src/js/components/AllCorrelations.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ComparisonTable from './ComparisonTable';
 import { connect } from 'react-redux';
 
 class AllCorrelations extends Component {
@@ -8,45 +9,11 @@ class AllCorrelations extends Component {
             <div>
                 <h2>Most positively correlated stocks :</h2>
                 <hr />
-                <table>
-                    <thead>
-                        <th>Ticker name</th>
-                        <th />
-                        <th>Value</th>
-                    </thead>
-                    <tbody>
-                        {allCorrelations.slice(0, 10).map(({ pair, value }) => (
-                            <tr>
-                                <td>{pair[0]}</td>
-                                <td>{pair[1]}</td>
-                                <td>
-                                    <p>{value}</p>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+                <ComparisonTable correlations={allCorrelations.slice(0, 10)} />
 
                 <h2>Least correlated stocks :</h2>
                 <hr />
-                <table>
-                    <thead>
-                        <th>Ticker name</th>
-                        <th />
-                        <th>Value</th>
-                    </thead>
-                    <tbody>
-                        {allCorrelations.slice(0, 10).map(({ pair, value }) => (
-                            <tr>
-                                <td>{pair[0]}</td>
-                                <td>{pair[1]}</td>
-                                <td>
-                                    <p>{value}</p>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+                <ComparisonTable correlations={allCorrelations.slice(-10)} />
             </div>
         );
     }
